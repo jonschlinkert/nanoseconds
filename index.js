8905fc6d383d1d30cafc119b3c7561834f9bd811
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-19 15:27:22 -0400

    run update, lint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /*!
  * nanoseconds <https://github.com/jonschlinkert/nanoseconds>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015-2018, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-module.exports = function (time) {
+module.exports = function(time) {
   if (!Array.isArray(time) || time.length !== 2) {
     throw new TypeError('expected an array from process.hrtime()');
   }
-  return ((+time[0]) * 1e9) + (+time[1]);
+  return +time[0] * 1e9 + +time[1];
 };
--- test.js
+++ test.js
@@ -2,22 +2,16 @@
 
 require('mocha');
 var assert = require('assert');
-var nanoseconds = require('./');
+var nano = require('./');
 
-describe('nanoseconds', function () {
-  it('should convert to nanoseconds:', function () {
-    assert.equal(nanoseconds([0, 999]), 999);
-    assert.equal(nanoseconds([12, 999]), 12000000999);
-    assert.equal(nanoseconds([1298001, 09187662]), 1298001009187662);
+describe('nanoseconds', function() {
+  it('should convert to nanoseconds:', function() {
+    assert.equal(nano([0, 999]), 999);
+    assert.equal(nano([12, 999]), 12000000999);
+    assert.equal(nano(['1298001', '09187662']), 1298001009187662);
   });
 
-  it('should throw an error:', function () {
-    try {
-      nanoseconds();
-    } catch(err) {
-      assert(err);
-      assert(err.message);
-      assert(err.message === 'expected an array from process.hrtime()');
-    }
+  it('should throw an error:', function() {
+    assert.throws(() => nano(), /expected an array from process\.hrtime()/)
   });
 });
